82fa29a8fa711fd39ff3281a156926f61ac4163b
Nolan <dongnguyenvie@gmail.com>
2022-04-28 15:10:24 +0700

    fixed: build config for production env

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
+import { Buffer } from 'buffer'
+window.Buffer = window.Buffer || Buffer
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
