fc6cc411b1316f96f3752342530373134bf59b10
xuanhungptithcm <xuanhungptithcm@gmail.com>
2022-05-01 16:13:08 +0700

    update path

--- src/pages/Access.tsx
+++ src/pages/Access.tsx
@@ -1,14 +1,5 @@
-import { Title } from '#components/Title'
-import { CardAsset } from '#modules/Access/CardAsset/CardAsset'
-import { Tabs } from '#modules/Access/Tab/Tabs'
 import React from 'react'
 
 export default function Access() {
-  return (
-    <div>
-      <Title title="My Assets" />
-      <CardAsset />
-      <Tabs color="pink" />
-    </div>
-  )
+  return <div>Access</div>
 }
